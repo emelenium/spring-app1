01f66e781161e12dae4d9755683ecb38bd9bf94b
anton <emelenium@gmail.com>
2020-04-18 23:13:16 +0500

    MusicPlayer и ref ссылки одного бина на другой

---
+++ src/main/java/ru/anton/springcourse/Music.java
@@ -0,0 +1,5 @@
+package ru.anton.springcourse;
+
+public interface Music {
+    String getSong();//вызывается у любого жанра музыки, возвращает одну песню заданного жанра
+}
--- src/main/java/ru/anton/springcourse/TestSpring.java
+++ src/main/java/ru/anton/springcourse/TestSpring.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+/*
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+*/
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
         context.close();
-        testBean.setName("Victory");
-        System.out.println(testBean.getName());
 
     }
 }
