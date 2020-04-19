662c2a4bfe78c32d006b43fefc264dc0b56c509f
anton <emelenium@gmail.com>
2020-04-19 22:34:24 +0500

    musicPlayer.properties

--- src/main/java/ru/anton/springcourse/MusicPlayer.java
+++ src/main/java/ru/anton/springcourse/MusicPlayer.java
@@ -1,6 +1,10 @@
 package ru.anton.springcourse;
 public class MusicPlayer {
 private Music music;
+private String name;
+private int volume;
+
+
 //IoC
     public MusicPlayer(Music music) {
         this.music = music;
@@ -11,5 +15,21 @@ private Music music;
     }
     public void playMusic(){
         System.out.println("Playing: "+ music.getSong());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
