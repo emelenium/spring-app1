350158a3c078173bf31cda136fdbdab8a167263b
emelenium <emelenium@gmail.com>
2021-01-25 15:04:22 +0500

    ClassicalMusik & RockMusik

--- src/main/java/ru/anton/springcourse/MusicPlayer.java
+++ src/main/java/ru/anton/springcourse/MusicPlayer.java
@@ -8,22 +8,27 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 //private List<Music> musicList = new ArrayList<>();
-private ClassicalMusic music;
+
+private ClassicalMusic classicalMusic;
+private RockMusic rockMusic;
 private String name;
 private int volume;
 
 
 //IoC
-    @Autowired
-    public MusicPlayer(ClassicalMusic music) {
-        this.music = music;
+@Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
     public MusicPlayer(){};
-    public void setMusic(ClassicalMusic music){
-        this.music = music;
-    }
+
+
     public void playMusic(){
-        System.out.println("Playing: "+ music.getSong());
+
+        System.out.println("Playing: "+ classicalMusic.getSong());
+        System.out.println("Playing: "+ rockMusic.getSong());
+
     }
 
     public String getName() {
