344d2e52385155a2db63b34f3a24e070f204b96d
emelenium <emelenium@gmail.com>
2021-01-25 15:28:40 +0500

    @Qualifier

--- src/main/java/ru/anton/springcourse/MusicPlayer.java
+++ src/main/java/ru/anton/springcourse/MusicPlayer.java
@@ -1,58 +1,17 @@
 package ru.anton.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 @Component
 public class MusicPlayer {
-//private List<Music> musicList = new ArrayList<>();
-
-private ClassicalMusic classicalMusic;
-private RockMusic rockMusic;
-private String name;
-private int volume;
-
-
-//IoC
-@Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
-    public MusicPlayer(){};
-
+    @Autowired
+    @Qualifier ("musicBean")
+    private Music music;
 
     public String playMusic(){
-        return "Playing: "+ classicalMusic.getSong();
-        //System.out.println("Playing: "+ classicalMusic.getSong());
-        //System.out.println("Playing: "+ rockMusic.getSong());
+        return "Playing: "+ music.getSong();
 
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
- /*   public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    public void playMusicList() {
-        for(Music music : musicList) {
-            System.out.println("Playing: " + music.getSong());
-        }
-    }*/
 }
